7ad401efe531b566d666b3e72becbd93c2dcaf03
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-01-11 17:42:51 +0530

    Add DateExtension class, GetOrdinalSuffix extension method and its related unit tests

---
+++ Microsoft.CSharp.Extensions.Tests/DateExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.CSharp.Extensions.Tests
+{
+    [TestClass]
+    public class DateExtensionsTests
+    {
+        #region GetOrdinalSuffix
+
+        [TestMethod]
+        public void GetOrdinalSuffix_1st_Test()
+        {
+            var input = new DateTime(2000, 01, 01).GetOrdinalSuffix();
+            Assert.IsTrue(input == "1st");
+        }
+
+        [TestMethod]
+        public void GetOrdinalSuffix_2nd_Test()
+        {
+            var input = new DateTime(2000, 01, 02).GetOrdinalSuffix();
+            Assert.IsTrue(input == "2nd");
+        }
+
+        [TestMethod]
+        public void GetOrdinalSuffix_3rd_Test()
+        {
+            var input = new DateTime(2000, 01, 03).GetOrdinalSuffix();
+            Assert.IsTrue(input == "3rd");
+        }
+
+        [TestMethod]
+        public void GetOrdinalSuffix_4th_Test()
+        {
+            var input = new DateTime(2000, 01, 04).GetOrdinalSuffix();
+            Assert.IsTrue(input == "4th");
+        } 
+
+        #endregion
+    }
+}
---
+++ Microsoft.CSharp.Extensions/DateExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CSharp.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Get the ordinal suffix for a given date
+        /// </summary>
+        /// <param name="datetime">Input date</param>
+        /// <returns>Ordinal suffix of a given input date</returns>
+        public static string GetOrdinalSuffix(this DateTime datetime)
+        {
+            int day = datetime.Day;
+
+            if (day % 100 >= 11 && day % 100 <= 13)
+                return String.Concat(day, "th");
+
+            switch (day % 10)
+            {
+                case 1:
+                    return String.Concat(day, "st");
+                case 2:
+                    return String.Concat(day, "nd");
+                case 3:
+                    return String.Concat(day, "rd");
+                default:
+                    return String.Concat(day, "th");
+            }
+        }
+    }
+}
