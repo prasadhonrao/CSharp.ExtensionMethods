1b587b9fe2eb65c2aef82c8f154f72de2c69114a
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-01-23 16:44:25 +0530

    Add IntExtensions class, Factorial, ReverseDigits extension methods and its related unit tests

---
+++ Microsoft.CSharp.Extensions.Tests/IntExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace Microsoft.CSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class IntExtensionsTests
+    {
+        #region Factorial
+
+        [Test]
+        public void Factorial_Valid_Int_Test()
+        {
+            var result = 5.Factorial();
+            Assert.AreEqual(120, result);
+        }
+
+        [Test]
+        public void Factorial_Zero_Test()
+        {
+            var result = 0.Factorial();
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Factorial_One_Test()
+        {
+            var result = 1.Factorial();
+            Assert.AreEqual(1, result);
+        }
+
+
+        [Test]
+        public void Factorial_Long_Number_Test()
+        {
+            var result = 10.Factorial();
+            Assert.AreEqual(3628800, result);
+        }
+
+        #endregion
+
+        #region ReverseDigits
+
+        [Test]
+        public void ReverseDigits_Valid_Int_Test()
+        {
+            var result = 12345.ReverseDigits();
+            Assert.AreEqual(54321, result);
+        }
+
+        [Test]
+        public void ReverseDigits_Zero_Test()
+        {
+            var result = 0.ReverseDigits();
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void ReverseDigits_SameNumber_Test()
+        {
+            var result =  111.ReverseDigits();
+            Assert.AreEqual(111, result);
+        }
+
+        #endregion
+    }
+}
---
+++ Microsoft.CSharp.Extensions/IntExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Microsoft.CSharp.Extensions
+{
+    public static class IntExtensions
+    {
+        #region Factorial
+
+        public static long Factorial(this int input)
+        {
+            long factorial = 1;
+            while (input > 0)
+            {
+                factorial = factorial * input;
+                input = input - 1;
+            }
+            return factorial;
+        }
+
+        #endregion
+
+        #region ReverseDigits
+
+        public static int ReverseDigits(this int input)
+        {
+            char[] digits = input.ToString().ToCharArray();
+            Array.Reverse(digits);
+            string newDigits = new string(digits);
+            return int.Parse(newDigits);
+        } 
+
+        #endregion
+    }
+}
