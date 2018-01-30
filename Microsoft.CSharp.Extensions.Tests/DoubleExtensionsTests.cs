8b7eb71eb006a9280869f60bad1cbd59ecfb6ef1
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-01-30 19:54:34 +0530

    Add DoubleExtension class, ToCurrency extension method and its related unit tests

---
+++ Microsoft.CSharp.Extensions.Tests/DoubleExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Microsoft.CSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class DoubleExtensionsTests
+    {
+        #region ToCurrency
+
+        [Test]
+        public void ToCurrency_EN_US_Test()
+        {
+            double value = 154.20;
+            string result = value.ToCurrency("en-US");
+            Assert.AreEqual("$154.20", result);
+        }
+
+        [Test]
+        public void ToCurrency_EN_GB_Test()
+        {
+            double value = 154.20;
+            string result = value.ToCurrency("en-GB");
+            Assert.AreEqual("£154.20", result);
+        }
+
+        [Test]
+        public void ToCurrency_HI_IN_Test()
+        {
+            double value = 1234.50;
+            string result = value.ToCurrency("hi-IN");
+            Assert.AreEqual("₹ 1,234.50", result);
+        }
+
+        #endregion
+    }
+}
---
+++ Microsoft.CSharp.Extensions/DoubleExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Microsoft.CSharp.Extensions
+{
+    /// <summary>
+    /// Double Extension Type
+    /// </summary>
+    public static class DoubleExtensions
+    {
+        #region ToCurrency
+
+        /// <summary>
+        /// Convert a double to string formatted using the specified culture
+        /// </summary>
+        /// <param name="value">Double value to convert</param>
+        /// <param name="cultureName">Culture information</param>
+        /// <returns></returns>
+        public static string ToCurrency(this double value, string cultureName)
+        {
+            CultureInfo currentCulture = new CultureInfo(cultureName);
+            return (string.Format(currentCulture, "{0:C}", value));
+        } 
+
+        #endregion
+    }
+}
