f7761d25b9e2e52d0b6cf1300bcde09b7d6d865c
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-02-05 13:12:11 +0530

    Add GenericExtensions class, ToJson method and its related unit tests

---
+++ Microsoft.CSharp.Extensions.Tests/GenericExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Microsoft.CSharp.Extensions.Tests
+{
+    [TestFixture]
+    public class GenericExtensionsTests
+    {
+        #region ToJson
+
+        [Test]
+        public void ToJson_Empty_String_Input_Test()
+        {
+            var result = string.Empty.ToJson();
+            Assert.IsTrue(result == string.Empty);
+        }
+
+        [Test]
+        public void ToJson_String_Input_Test()
+        {
+            var result = "hello world".ToJson();
+            Assert.IsTrue(result == "hello world");
+        }
+
+        [Test]
+        public void ToJson_Anonymous_Type_Input_Test()
+        {
+            var p = new { FirstName = "Prasad", LastName = "Honrao"};
+            var result = p.ToJson();
+            Assert.IsTrue(result == "{\"FirstName\":\"Prasad\",\"LastName\":\"Honrao\"}");
+        }
+
+        #endregion
+    }
+}
---
+++ Microsoft.CSharp.Extensions/GenericExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.CSharp.Extensions
+{
+    /// <summary>
+    /// Generic Extension Type
+    /// </summary>
+    public static class GenericExtensions
+    {
+        /// <summary>
+        /// ToJson converts a given object representation to JSON format
+        /// </summary>
+        /// <typeparam name="T">Generic input parameter</typeparam>
+        /// <param name="input">JSON representation of input value</param>
+        /// <returns></returns>
+        public static string ToJson<T>(this T input)
+        {
+            return input != null ? JsonConvert.SerializeObject(input) : null;
+        }
+    }
+}
