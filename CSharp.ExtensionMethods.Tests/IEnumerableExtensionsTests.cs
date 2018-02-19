1a163b368334dc5df33a6d19fd3ac136777c9b94
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-02-19 11:18:30 +0530

    Add IEnumerable extension method Shuffle and its related unit tests

---
+++ CSharp.ExtensionMethods.Tests/IEnumerableExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CSharp.ExtensionMethods.Tests
+{
+    [TestFixture]
+    public class IEnumerableExtensionsTests
+    {
+        #region Shuffle
+
+        [Test]
+        public void Shuffle_Integer_Collection_Test()
+        {
+            var result = new List<int>() { 1, 2, 3, 4, 5 }.Shuffle();
+            Assert.IsTrue(result.ToList().Count > 0);
+        }
+
+        [Test]
+        public void Shuffle_String_Collection_Test()
+        {
+            var result = new List<string>() { "a", "b", "c", "d", "e" }.Shuffle();
+            Assert.IsTrue(result.ToList().Count > 0);
+        }
+
+        [Test]
+        public void Shuffle_Mixed_Collection_Test()
+        {
+            var result = new List<object>() { "a", 1, new List(), DateTime.Now, null }.Shuffle();
+            Assert.IsTrue(result.ToList().Count > 0);
+        }
+
+        [Test]
+        public void Shuffle_Null_Collection_Test()
+        {
+            var nullList = new List<int>();
+            nullList = null;
+            Assert.That(() => nullList.Shuffle(), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        #endregion
+    }
+}
---
+++ CSharp.ExtensionMethods/IEnumerableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.ExtensionMethods
+{
+    public static class IEnumerableExtensions
+    {
+        #region Shuffle
+
+        static public IEnumerable<T> Shuffle<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null) throw new ArgumentNullException("Input collection cannot be null");
+
+            return ShuffleIterator(collection);
+        }
+
+        static private IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> source)
+        {
+            T[] array = source.ToArray();
+            Random rnd = new Random();
+            for (int n = array.Length; n > 1;)
+            {
+                int k = rnd.Next(n--); // 0 <= k < n
+
+                //Swap items
+                if (n != k)
+                {
+                    T tmp = array[k];
+                    array[k] = array[n];
+                    array[n] = tmp;
+                }
+            }
+
+            foreach (var item in array) yield return item;
+        } 
+
+        #endregion
+    }
+}
